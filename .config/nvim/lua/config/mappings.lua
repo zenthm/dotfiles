664fc55344fbd149f9f1d830d5c319820a95ff04
Zenthm <zenthm.dev@gmail.com>
2024-02-17 15:45:59 +0800

    Add mappings for nvim-tree

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -15,6 +15,13 @@ mappings = {
     ["<leader>q"] = { "<cmd>q<cr>", "Quit" },
     ["<leader>Q"] = { "<cmd>q!<cr>", "Force quit" },
     ["<leader>k"] = { "<cmd>WhichKey<cr>", "Show WhichKey" },
+    ["<leader>t"] = {
+        name = "+nvim-tree",
+        ["o"] = { "<cmd>NvimTreeOpen<cr>", "Open nvim-tree" },
+        ["x"] = { "<cmd>NvimTreeClose<cr>", "Close nvim-tree" },
+        ["t"] = { "<cmd>NvimTreeToggle<cr>", "Toggle nvim-tree" },
+        ["f"] = { "<cmd>NvimTreeFocus<cr>", "Focus nvim-tree" },
+    },
 }
 
 return mappings
