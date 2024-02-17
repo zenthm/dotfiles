a3fe392b21f8e886e1492c9a057ed35266a20111
Zenthm <zenthm.dev@gmail.com>
2024-02-17 11:33:02 +0800

    Update nvim-tree options

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -1,6 +1,8 @@
+-- Set the leader key for mappings
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Define key mappings for Tmux navigation
 vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
 vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
 vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
