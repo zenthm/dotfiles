8de313076ed6d5bd6dc535e9b43603ca78d90a4d
Zenthm <zenthm.dev@gmail.com>
2024-02-17 10:55:20 +0800

    Update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,5 +5,7 @@ require("config.bootstrap")
 require("config.options")
 require("config.mappings")
 
--- Setup lazy.nvim for plugin management
-require("lazy").setup("config.plugins")
+-- Setup lazy.nvim to install plugins
+lazy_plugins = require("config.plugins")
+lazy_config = require("config.lazy")
+require("lazy").setup(lazy_plugins, lazy_config)
--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -1,3 +1,12 @@
 return {
     "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        config = function()
+            vim.cmd.colorscheme("catppuccin")
+        end,
+        priority = 1000,
+    },
 }
