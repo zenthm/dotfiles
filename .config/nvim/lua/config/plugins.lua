4ebc77459dca5e0117adda6bb495dea09e20878a
Zenthm <zenthm.dev@gmail.com>
2024-02-17 15:13:27 +0800

    Add bufferline nvim plugin

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -42,8 +42,28 @@ return {
     },
     {
         "nvim-lualine/lualine.nvim",
-        lazy = false,
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {},
+        event = "VeryLazy",
+    },
+    {
+        "akinsho/bufferline.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            options = {
+                mode = "buffers",
+                close_command = "Bdelete! %d",
+                right_mouse_command = "Bdelete! %d",
+                left_mouse_command = "buffer %d",
+                offsets = {
+                    {
+                        filetype = "NvimTree",
+                        text = "File Explorer",
+                        text_align = "center",
+                    },
+                },
+            },
+        },
+        event = "VeryLazy",
     },
 }
