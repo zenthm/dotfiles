4409d2e1edabc8c1922a844b8d114679b3be79fc
Zenthm <zenthm.dev@gmail.com>
2024-02-19 10:42:28 +0800

    Add nvim cmp plugin

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -162,6 +162,56 @@ return {
         build = ":TSUpdate",
         event = { "BufNewFile", "BufReadPre", "VeryLazy" },
     },
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+        },
+        opts = {},
+        config = function(_, opts)
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            local select_opts = { behavior = cmp.SelectBehavior.Select }
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                sources = {
+                    { name = "path" },
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "luasnip" },
+                },
+                mapping = {
+                    ["<Up>"] = cmp.mapping.select_prev_item(select_opts),
+                    ["<Down>"] = cmp.mapping.select_next_item(select_opts),
+                    ["<Esc>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm(),
+                },
+            })
+        end,
+        event = { "BufNewFile", "BufReadPre", "VeryLazy" },
+    },
     {
         "andweeb/presence.nvim",
         opts = {},
