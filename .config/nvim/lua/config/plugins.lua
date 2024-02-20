0de42398ca4944c1097e25d54c66fc1cae956e78
Zenthm <zenthm.dev@gmail.com>
2024-02-20 10:44:18 +0800

    Configure nvim lspconfig

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -74,6 +74,42 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
+        config = function(_, opts)
+            require("neodev")
+
+            local lspconfig = require("lspconfig")
+            local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            lsp_capabilities.textDocument.foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
+            }
+
+            lspconfig.clangd.setup({
+                capabilities = lsp_capabilities,
+            })
+            lspconfig.cmake.setup({
+                capabilities = lsp_capabilities,
+            })
+            lspconfig.cssls.setup({
+                capabilities = lsp_capabilities,
+            })
+            lspconfig.html.setup({
+                capabilities = lsp_capabilities,
+            })
+            lspconfig.lua_ls.setup({
+                capabilities = lsp_capabilities,
+            })
+            lspconfig.pyright.setup({
+                capabilities = lsp_capabilities,
+            })
+            lspconfig.ruby_ls.setup({
+                capabilities = lsp_capabilities,
+            })
+            lspconfig.tsserver.setup({
+                capabilities = lsp_capabilities,
+            })
+        end,
         event = { "BufNewFile", "BufReadPre", "VeryLazy" },
     },
     {
