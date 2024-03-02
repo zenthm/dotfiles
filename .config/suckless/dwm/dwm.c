e31e0c66b4aeaea8aeaa286c2a20b2d6bed42b39
Zenthm <zenthm.dev@gmail.com>
2024-03-02 11:50:05 +0800

    Apply patch dwm actualfullscreen

--- .config/suckless/dwm/dwm.c
+++ .config/suckless/dwm/dwm.c
@@ -221,6 +221,7 @@ static void tagtoprev(const Arg *arg);
 static void tile(Monitor *m);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void freeicon(Client *c);
@@ -1853,6 +1854,13 @@ togglefloating(const Arg *arg)
 		resize(selmon->sel, selmon->sel->x, selmon->sel->y,
 			selmon->sel->w, selmon->sel->h, 0);
 	arrange(selmon);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+  if(selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
